fe155d4e8a0a561033329bccd8d4b1b880f61180
Dolsy Smith <dsmith@gwu.edu>
2023-07-27 13:21:10 -0400

    Lesson formatting & revision

--- course-utils/postprocessing.py
+++ course-utils/postprocessing.py
@@ -4,17 +4,23 @@ from copy import deepcopy
 import click
 from pathlib import Path
 import logging
+from itertools import dropwhile
 
 logging.basicConfig()
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
+# Pattern that offsets myST directives in the notebook JSON
 DIRECTIVE_QUOTES = '````'
 # Pattern for an {image} directive followed by a URL
 IMAGE_PATTERN = re.compile('````{image}\s?(.+)\n') 
+# Pattern for an {admonition} directive followed by a tag
+ADMON_PATTERN = re.compile('````{admonition}\s?(.+)\n')
+# heading level to apply to admonitions, etc.
+HEADING_SUB_LEVEL = 4
 # Cells with tags in this list will be removed
 TAGS_TO_REMOVE = {'instructor', 'remove-cell'}
-
 
 class Notebook:
 
@@ -31,16 +37,23 @@ class Notebook:
         Removes MyST directives from the notebook's markdown cells. Assumes such directives are enclosed in four backticks (as opposed to three for code blocks.) Leaves the inner Markdown intact. In the case of the {image} directives, replaces the content with a standard inline image reference.
         '''
         for i, cell in enumerate(self.nb_json['cells']):
-            # Assumes the directive encloses the entire cell
-            if cell['cell_type'] == 'markdown' and cell['source'][0].startswith(DIRECTIVE_QUOTES):
-                # If image directive, extract URL and construct standard markdown expression
-                image_match = IMAGE_PATTERN.match(cell['source'][0])
+            # Assumes the directive encloses the entire cell, excluding any blank initial lines
+            cell_content = list(dropwhile(lambda x: not x or x.isspace(), cell['source']))
+            if cell['cell_type'] == 'markdown' and cell_content[0].startswith(DIRECTIVE_QUOTES):
+                image_match = IMAGE_PATTERN.match(cell_content[0])
+                admon_match = ADMON_PATTERN.match(cell_content[0])
                 if image_match:
-                    image_url = image_match.group(1)
-                    self.nb_json['cells'][i]['source'] = [f'![Flow chart with three boxes, one labeled Python Camp, one labeled Instructor, and one labeled Learner. The Python Camp box is connected to the other two with arrows labeled \"has one or more\"]({image_url})']
-                else:
-                    # Otherwise, remove enclosing backticks and directive
-                    self.nb_json['cells'][i]['source'] = cell['source'][1:-1]
+                        # Extract alt text if present
+                        alt_tag = [c for c in cell_content if c.startswith(':alt:')]
+                        alt_text = alt_tag[0].replace(':alt: ', '') if alt_tag else ''
+                        self.nb_json['cells'][i]['source'] = [f'![{alt_text}]({image_match.groups(1)})']
+                        continue
+                elif admon_match:
+                    # Replace admonition with appropriate level of Markdown heading
+                    cell_content[0] = '#' * HEADING_SUB_LEVEL + f' {admon_match.groups(1)}'
+                # Omit last line (closing backticks for directive) and any CSS definitions
+                cell_content = [c for c in cell_content if not c.startswith(DIRECTIVE_QUOTES) and not c.startswith(':class:')]
+                self.nb_json['cells'][i]['source'] = cell_content
         return self
     
     def remove_tagged_cells(self, tags=TAGS_TO_REMOVE):
